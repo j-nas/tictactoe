bb7fdd88372c5fe032b4679d5f3572f927cff28e
Jon Naske <jonnaske@hotmail.com>
2021-03-26 02:09:36 -0700

    avatar selection javascript complete

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-2021-03-24 12:42:00
-===================
-Initial commit
+2021-03-26 01:31:28
+/=================/
+Avatar selection code complete.
 
 2021-03-25 02:22:55
-===================
+/=================/
 Reworked victory condition logic, now all possible combinations are accounted for. 
 
 Todo: GUI rework, avatar selection, and todo list
+
+2021-03-24 12:42:00
+/=================/
+Initial commit
+
--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-const createPlayer = (name, marker) => {
-  return { name, marker };
-};
+const player = (() => {
+  function Avatar(emoji, description, datasq) {
+    this.emoji = emoji
+    this.description = description
+    this.datasq = datasq
+  }
+  const fec = new Avatar("💩", "Foul", 0)
+  const x = new Avatar("❌", "The classic", 8)
+  const toilet = new Avatar('🚽', "Powerflush", 1)
+  const taco = new Avatar("🌮", "The anti-hero", 6)
+  const stoneguy = new Avatar("🗿", "The stone guy", 3)
+  const guitar = new Avatar("🎸", "🤘🤘🤘🤘🤘", 5)
+  const peach = new Avatar("🍑", "Peachy", 7)
+  const beer = new Avatar("🍻", "Cheers!", 4)
+  const lit = new Avatar("🔥", "Lit", 9)
+  const nope = new Avatar("🚫", "Nope", 2)
+  let currentAvatar = x
+  const getAvatarList = () => {
+    return [x, toilet, taco, stoneguy, guitar, peach, beer, lit, nope]
+  }
+  const setCurrentAvatar = (dataav) => {
+    currentAvatar = getAvatarList().find(obj => obj.datasq == dataav)
+    
+    
+  }
+  const getCurrentAvatar = () => {
+    return currentAvatar
+  }
+  const getOpponentAvatar = () => {
+    return fec
+  }
+  return {
+    
+    getAvatarList,
+    setCurrentAvatar,
+    getCurrentAvatar,
+    getOpponentAvatar,
+  }
+})()
 
 const gameBoard = (() => {
   let turn = "X";
@@ -46,7 +82,7 @@ const gameBoard = (() => {
         }
         if (count == 3) {
           drawBoard.lockBoard()
-          return alert("PLAYER " + getTurn() + "WINS!")
+          return alert("PLAYER " + getTurn() + " WINS!")
         }
       }
     }
@@ -88,13 +124,11 @@ const gameBoard = (() => {
 })();
 
 const drawBoard = (() => {
-  let playerX = "❌"
-  let playerO = "⭕"
   let squares = document.querySelectorAll(".square");
   const clearBoard = () => {
-    squares.forEach((div) => {
-      div.textContent = "";
-      div.removeAttribute("style")
+    squares.forEach((e) => {
+      e.textContent = "";
+      e.removeAttribute("style")
     });
   };
   const lockBoard = () => {
@@ -106,22 +140,57 @@ const drawBoard = (() => {
   const updateBoard = () => {
     for (let i = 0; i < gameBoard.getBoardO().length; i++) {
       document.querySelector(`[data-sq="${gameBoard.getBoardO()[i]}"]`)
-        .textContent = playerO;
+        .textContent =  player.getOpponentAvatar().emoji
     }
     for (let j = 0; j < gameBoard.getBoardX().length; j++) {
       document.querySelector(`[data-sq="${gameBoard.getBoardX()[j]}"]`)
-        .textContent = playerX;
+        .textContent =  player.getCurrentAvatar().emoji;
     }
   };
-  squares.forEach((e) => {
+
+   
+  
+  
+  
+  
+  
+  const avatarSel = () => {
+  
+    gameBoard.reset()    
+    for (let i = 0; i < player.getAvatarList().length; i++) {//loop to create 
+      let innerSquare = document.createElement("div")        //selection squares
+      innerSquare.setAttribute(`data-av`, `${player.getAvatarList()[i].datasq}`)
+      innerSquare.classList.add("innerSquare")
+      innerSquare.innerText = `${player.getAvatarList()[i].emoji}`
+      
+      document.querySelector(`[data-sq="${player.getAvatarList()[i].datasq}"]`).appendChild(innerSquare)
+    }
+    let innerSquare = document.querySelectorAll(".innerSquare")
+    innerSquare.forEach((e) => {
+      e.addEventListener("click", () => {
+        player.setCurrentAvatar(+e.dataset.av)
+        console.log(player.getCurrentAvatar())
+      })
+    })
+    
+  }
+
+  squares.forEach((e) => { //regular gameplay listener
     e.addEventListener("click", () => {
       gameBoard.playerPlay(+e.dataset.sq);
       e.style.pointerEvents = "none";
     });
   });
+  const getSquares = () => {
+    return (squares)
+  }
   return {
+    getSquares,
     clearBoard,
     updateBoard,
-    lockBoard
+    lockBoard,
+    avatarSel,
+
+   
   };
 })();
